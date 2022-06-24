9d78fcd60a29a70b7abee5dc19837d9a94a5744a
Matheus Pinheiro <17935077+mp-pinheiro@users.noreply.github.com>
2022-06-24 17:31:39 -0300

    Paths suck

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { Button, ButtonGroup } from '@mui/material';
 import ImageComponent from './components/ImageComponent';
 import React from 'react';
 
-require.context('./assets', false, /\.(png|jpe?g|svg)$/);
-
 function importAll(r) {
     let images = {};
     r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
     return images
 }
-const images = importAll(require.context('./assets', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(require.context('../public/assets', false, /\.(png|jpe?g|svg)$/));
 
 function App() {
     // choose a random image from the images object
--- src/components/ImageComponent.js
+++ src/components/ImageComponent.js
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 
 class ImageComponent extends React.Component {
     render() {
-        const path = `../assets/${this.props.src}`
+        const path = `/assets/${this.props.src}`
         console.log(__dirname)
         return (
             <Box component="span" sx={{ p: 2, width: '25%', height: '25%' }}>
