8d6dc1b1859bfb9baf87c815deb0409aff538cc5
Matheus Pinheiro <17935077+mp-pinheiro@users.noreply.github.com>
2022-06-24 17:16:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, ButtonGroup } from '@mui/material';
+import ImageComponent from './components/ImageComponent';
+import React from 'react';
+
+require.context('./assets', false, /\.(png|jpe?g|svg)$/);
+
+function importAll(r) {
+    let images = {};
+    r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images
+}
+const images = importAll(require.context('./assets', false, /\.(png|jpe?g|svg)$/));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // choose a random image from the images object
+    const randomImage = Object.keys(images)[Math.floor(Math.random() * Object.keys(images).length)];
+
+    // build src path for image
+    const alt = '';
+
+    // initialize prop
+    const [srcProp, setSrcProp] = React.useState(randomImage);
+    const [altProp, setAltProp] = React.useState(alt);
+    const imageComponent = <ImageComponent src={srcProp} alt={altProp} />;
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {imageComponent}
+                <ButtonGroup variant="contained" aria-label="outlined primary button group">
+                    <Button>One</Button>
+                    <Button>Two</Button>
+                    <Button>Three</Button>
+                </ButtonGroup>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ImageComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+class ImageComponent extends React.Component {
+    render() {
+        const path = `../assets/${this.props.src}`
+        console.log(__dirname)
+        return (
+            <Box component="span" sx={{ p: 2, width: '25%', height: '25%' }}>
+                <img src={path} alt={this.props.alt} style={{ width: '100%', height: '100%' }} />
+            </Box>
+        );
+    }
+}
+
+export default ImageComponent;
